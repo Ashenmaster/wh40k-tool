4d42a8a95899c1768884ba80ff29baef1127e2f9
Chris Gray <cgray2792@gmail.com>
2019-04-08 08:45:01 +0100

    Add better logging

--- counter/index.js
+++ counter/index.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const addRequestID = require('express-request-id');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const status = require('./status');
 
 let app = express();
+// Function to set the logging level based on environment
+// TODO: Expand this to use a debugging env var
+function setLogging(environment) {
+	app.use(addRequestID());
+	morgan.token('id', function getId (req) {
+		return req.id;
+	});
 
-app.use(morgan('combined', {
-	skip: function (req, res) {
-		return res.statusCode < 400
+	let loggerFormat = ':id [:date[web]]" :method :url" :status :response-time;';
+	if(environment === 'dev' || environment === 'test') {
+		app.use(morgan(loggerFormat, {
+			stream: process.stderr
+		}));
+	} else {
+		app.use(morgan('combined', {
+			skip: function (req, res) {
+				return res.statusCode < 400;
+			}
+		}));
 	}
-}));
+}
+
+setLogging(process.env.ENVIRONMENT);
+
 app.use(bodyParser.json());
 
 function validateInput(value) {
@@ -52,6 +71,7 @@ app.post('/', (req, res) => {
 		}
 	} else {
 		res.sendStatus(400);
+
 	}
 
 
